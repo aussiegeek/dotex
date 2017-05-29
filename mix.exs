5f2a4add5379a82b0bfdc160206f65e74846f5e6
Alan Harper <alan@aussiegeek.net>
2017-05-29 17:12:21 +1000

    Add package information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Dotex.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package(),
+     description: "Generate Graphviz graphs from Elixir",
+     name: "Dotex",
+    ]
   end
 
   def application do
@@ -20,6 +24,15 @@ defmodule Dotex.Mixfile do
       {:credo, "~> 0.7", only: [:dev, :test]},
       {:ex_doc, "~> 0.14", only: :docs, runtime: false, optional: true},
       {:inch_ex, ">= 0.0.0", only: :docs, optional: true},
+    ]
+  end
+
+  defp package do
+    [
+      name: :dotex,
+      maintainers: ["Alan Harper"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/aussiegeek/dotex"}
     ]
   end
 end
